ac9753967564d5d94f6beeb807ce2d9a9696715f
Alua Baikadamova <abaikad00@gmail.com>
2018-12-17 16:59:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+if name_hash.empty?
+  exit
+end
+winner=1000000
+name_hash.each do |name, number|
+  if (number<winner)
+    winner = number
+  end
+end
+
+name_hash.each do |name, number|
+  if (number==winner)
+    return name
+  end
+end
 end
